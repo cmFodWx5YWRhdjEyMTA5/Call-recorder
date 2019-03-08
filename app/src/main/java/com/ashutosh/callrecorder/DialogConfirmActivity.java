7c9ecfc40af2b11f0211b8100d8c8e4e226fbcaa
Ashutosh Chaudhary <imashutoshchaudhary@gmail.com>
2019-03-08 12:18:06 +0530

    Call Recorder Android App

---
+++ app/src/main/java/com/ashutosh/callrecorder/DialogConfirmActivity.java
@@ -0,0 +1,37 @@
+package com.ashutosh.callrecorder;
+
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+
+import com.ashutosh.callrecorder.service.RecordService;
+
+public class DialogConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_confirm_speaker);
+
+        AppCompatButton yesButton = (AppCompatButton) findViewById(R.id.yesBt);
+        AppCompatButton noButton = (AppCompatButton) findViewById(R.id.noBt);
+
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RecordService.audioManager.setMode(AudioManager.MODE_IN_CALL);
+                RecordService.audioManager.setSpeakerphoneOn(true);
+                finish();
+            }
+        });
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
